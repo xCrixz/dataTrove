a61865628b06225f0ecf6d6ae53d85b560b9940a
xCrixz <sharzapps@gmail.com>
2025-11-17 07:06:55 -0500

    Se agregarón 62 nuevos datos para la categoria Historia

--- app/src/main/java/com/sharkzapps/datatrove/pantallas/datos/DatosHistoria.kt
+++ app/src/main/java/com/sharkzapps/datatrove/pantallas/datos/DatosHistoria.kt
@@ -1,2 +1,84 @@
 package com.sharkzapps.datatrove.pantallas.datos
 
+const val dato1Historia = "La Gran Muralla China no es visible desde el espacio a simple vista."
+const val dato2Historia = "Alejandro Magno creó uno de los imperios más grandes antes de los 30 años."
+const val dato3Historia = "En la Edad Media se creía que bañarse era dañino para la salud."
+const val dato4Historia = "El Imperio Romano duró más de 1.000 años."
+const val dato5Historia = "El café fue prohibido en Meca en el siglo XVI por considerarse " +
+            "una bebida \"rebelde\"."
+const val dato6Historia = "En la antigua Grecia, los atletas competían desnudos."
+const val dato7Historia = "Japón tuvo samuráis durante casi 700 años."
+const val dato8Historia = "Los primeros relojes mecánicos aparecieron en el siglo XIII."
+const val dato9Historia = "En el Antiguo Egipto, los gatos eran considerados animales sagrados."
+const val dato10Historia = "La Torre Eiffel originalmente iba a ser una estructura temporal."
+const val dato11Historia = "Los romanos usaban orina para lavar ropa."
+const val dato12Historia = "En la Edad Media, los caballeros podían tardar una hora " +
+            "en ponerse la armadura."
+const val dato13Historia = "La peste negra mató a más de un tercio de la población europea."
+const val dato14Historia = "El primer mapa del mundo conocido fue creado en Babilonia."
+const val dato15Historia = "En la antigua Roma, los gladiadores casi nunca luchaban a muerte."
+const val dato16Historia = "La Edad de Hielo terminó hace unos 11.700 años."
+const val dato17Historia = "La primera universidad del mundo fue fundada en el año 859 en Marruecos."
+const val dato18Historia = "En la Edad Media se creía que el tomate era venenoso."
+const val dato19Historia = "Los caballos fueron domesticados hace unos 5.500 años."
+const val dato20Historia = "El Imperio Persa llegó a abarcar tres continentes."
+const val dato21Historia = "La primera fotografía de la historia fue tomada en 1826."
+const val dato22Historia = "Los aztecas realizaban sacrificios para \"alimentar\" a sus dioses."
+const val dato23Historia = "El faraón Tutankamón murió a los 19 años."
+const val dato24Historia = "Las katanas podían tardar meses en forjarse."
+const val dato25Historia = "Los chinos usaban pólvora mucho antes de que llegara a Europa."
+const val dato26Historia = "El fuego fue controlado por los humanos hace más de 1 millón de años."
+const val dato27Historia = "El Titanic tardó más de 2 horas en hundirse."
+const val dato28Historia = "Isaac Newton inventó el cálculo y la ley de gravedad cuando tenía 23 años."
+const val dato29Historia = "La Ruta de la Seda conectaba Asia y Europa desde hace 2.000 años."
+const val dato30Historia = "Los alquimistas buscaban convertir metales comunes en oro."
+const val dato31Historia = "Napoleón medía alrededor de 1.68 m"
+const val dato32Historia = "El Imperio Español fue el primer imperio global de la historia moderna."
+const val dato33Historia = "Los griegos antiguos creían que los sueños eran mensajes de los dioses."
+const val dato34Historia = "El Código de Hammurabi es una de las leyes más antiguas del mundo."
+const val dato35Historia = "Los faraones eran enterrados con tesoros para su vida después de la muerte."
+const val dato36Historia = "En la Edad Media, los libros eran tan valiosos " +
+            "que se encadenaban a las mesas."
+const val dato37Historia = "Los guerreros espartanos empezaban su entrenamiento a los 7 años."
+const val dato38Historia = "La Gran Depresión de 1929 afectó a casi todo el mundo."
+const val dato39Historia = "El Imperio Otomano duró más de 600 años."
+const val dato40Historia = "El Renacimiento comenzó en Italia debido a su riqueza comercial."
+const val dato41Historia = "Los humanos llegaron a América hace más de 15.000 años."
+const val dato42Historia = "Los sacerdotes egipcios afeitaban todo su cuerpo como signo de pureza."
+const val dato43Historia = "La Guerra Fría nunca se convirtió en un conflicto directo " +
+            "entre EE.UU. y la URSS."
+const val dato44Historia = "Los primeros bancos surgieron en la antigua Mesopotamia."
+const val dato45Historia = "Los mayas fueron una de las civilizaciones más precisas en astronomía."
+const val dato46Historia = "Gengis Kan creó el imperio contiguo más grande de la historia"
+const val dato47Historia = "Los espartanos consideraban deshonroso rendirse."
+const val dato48Historia = "En la antigua Grecia, el teatro surgió como parte de festivales religiosos."
+const val dato49Historia = "La imprenta de Gutenberg revolucionó el acceso al conocimiento."
+const val dato50Historia = "El Muro de Berlín se mantuvo en pie desde 1961 hasta 1989."
+const val dato51Historia = "Los vikingos usaban cebollas para saber la gravedad de las heridas."
+const val dato52Historia = "Alejandro Magno nunca perdió una batalla."
+const val dato53Historia = "La seda fue un secreto chino durante más de 2.000 años."
+const val dato54Historia = "En la Edad Media, mucha gente no sabía leer, pero " +
+            "memorizaba historias completas."
+const val dato55Historia = "Los mongoles tenían mensajeros que podían recorrer 200 km por día."
+const val dato56Historia = "Las mujeres egipcias tenían más derechos que muchas " +
+            "mujeres europeas medievales."
+const val dato57Historia = "Los soldados romanos cobraban parte de su sueldo en sal."
+const val dato58Historia = "La Gran Muralla China mide más de 21.000 kilómetros."
+const val dato59Historia = "El café se popularizó primero en Medio Oriente antes que en Europa."
+const val dato60Historia = "Los caballeros medievales practicaban torneos como entrenamiento."
+const val dato661Historia = "La brújula permitió a los europeos explorar el mundo entero."
+const val dato62Historia = "Los griegos inventaron los Juegos Olímpicos hace más de 2.700 años."
+
+val datosHistoria = listOf(dato1Historia, dato2Historia, dato3Historia, dato4Historia,
+    dato5Historia, dato6Historia, dato7Historia, dato8Historia, dato9Historia,
+    dato10Historia, dato11Historia, dato12Historia, dato13Historia, dato14Historia,
+    dato15Historia, dato16Historia, dato17Historia, dato18Historia, dato19Historia,
+    dato20Historia, dato21Historia, dato22Historia, dato23Historia, dato24Historia,
+    dato25Historia, dato26Historia, dato27Historia, dato28Historia, dato29Historia,
+    dato30Historia, dato31Historia, dato32Historia, dato33Historia, dato34Historia,
+    dato35Historia, dato36Historia, dato37Historia, dato38Historia, dato39Historia,
+    dato40Historia, dato41Historia, dato42Historia, dato43Historia, dato44Historia,
+    dato45Historia, dato46Historia, dato47Historia, dato48Historia, dato49Historia,
+    dato50Historia, dato51Historia, dato52Historia, dato53Historia, dato54Historia,
+    dato55Historia, dato56Historia, dato57Historia, dato58Historia, dato59Historia,
+    dato60Historia, dato661Historia, dato62Historia)
