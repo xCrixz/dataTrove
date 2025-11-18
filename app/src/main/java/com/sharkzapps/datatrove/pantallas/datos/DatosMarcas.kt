546905b781794248dc52ea6b96b69908582fde28
xCrixz <sharzapps@gmail.com>
2025-11-18 09:12:33 -0500

    Se agregarón 55 nuevos datos para la categoria Marcas

--- app/src/main/java/com/sharkzapps/datatrove/pantallas/datos/DatosMarcas.kt
+++ app/src/main/java/com/sharkzapps/datatrove/pantallas/datos/DatosMarcas.kt
@@ -1,2 +1,82 @@
 package com.sharkzapps.datatrove.pantallas.datos
 
+const val dato1Marcas = "Coca-Cola es la segunda palabra más reconocida del mundo."
+const val dato2Marcas = "Puma fue creada por el hermano del fundador de Adidas " +
+            "tras una pelea familiar."
+const val dato3Marcas = "LEGO es la marca de juguetes más poderosa del mundo."
+const val dato4Marcas = "McDonald’s originalmente vendía hot dogs, no hamburguesas."
+const val dato5Marcas = "Nike obtuvo su logo por solo \$35 dólares"
+const val dato6Marcas = "Pepsi estaba fabricada originalmente para ayudar con la digestión"
+const val dato7Marcas = "Samsung empezó como una empresa que vendía pescado seco y fideos."
+const val dato8Marcas = "Nintendo nació en 1889 vendiendo cartas para juegos tradicionales, " +
+            "no videojuegos."
+const val dato9Marcas = "Cuando Google nació, su nombre original era Backrub."
+const val dato10Marcas = "Starbucks se iba a llamar \"Cargo House\", pero cambiaron " +
+            "el nombre porque sonaba feo."
+const val dato11Marcas = "El logo de Ferrari proviene del símbolo que usaba un piloto " +
+            "italiano en la Primera Guerra Mundial."
+const val dato12Marcas = "Play-Doh originalmente era un limpiador de papel tapiz para casas."
+const val dato13Marcas = "Burger King en Australia se llama Hungry Jack’s."
+const val dato14Marcas = "KFC usa la imagen del Coronel Sanders porque el fundador realmente " +
+            "era coronel honorario en Kentucky."
+const val dato15Marcas = "Amazon eligió su nombre porque empezaba por “A” y aparecía " +
+            "primero en los listados."
+const val dato16Marcas = "El logo de Amazon tiene una flecha de la A a la Z porque venden \"de todo\"."
+const val dato17Marcas = "La mascota de Pringles se llama Julius Pringle."
+const val dato18Marcas = "La empresa Porsche creó el primer automóvil híbrido en 1900"
+const val dato19Marcas = "Ben & Jerry’s permite a sus empleados llevar a sus mascotas al trabajo."
+const val dato20Marcas = "El primer producto que vendió eBay fue un puntero láser roto " +
+            "y alguien lo compró igual."
+const val dato21Marcas = "El nombre Spotify nació por accidente: malinterpretaron " +
+            "una sugerencia que alguien gritó."
+const val dato22Marcas = "Rolex prueba sus relojes en cámaras de presión para simular " +
+            "profundidades oceánicas extremas."
+const val dato23Marcas = "La primera tienda de Zara se llamaba Zorba."
+const val dato24Marcas = "BMW fabricaba motores de avión antes que carros y motos."
+const val dato25Marcas = "Barbie mide exactamente 29 cm porque representa a una mujer de 1,75 m."
+const val dato26Marcas = "La fórmula de Coca-Cola está guardada en una bóveda a la que solo " +
+            "pueden entrar dos personas a la vez."
+const val dato27Marcas = "Los carros de Google Maps han sido perseguidos por animales, " +
+            "personas curiosas y hasta drones."
+const val dato28Marcas = "Crayola ha producido más de 120 mil millones de crayones desde 1903."
+const val dato29Marcas = "Disney es uno de los mayores compradores de fuegos artificiales del mundo."
+const val dato30Marcas = "Red Bull vende más latas en un año que la cantidad de personas " +
+            "que hay en todo el planeta."
+const val dato31Marcas = "PlayStation comenzó como un proyecto conjunto entre Sony y Nintendo."
+const val dato32Marcas = "Nike originalmente se llamaba Blue Ribbon Sports."
+const val dato33Marcas = "La primera Apple Store abrió en 2001."
+const val dato34Marcas = "Ferrari produce menos de 10.000 coches al año a propósito para " +
+            "mantener exclusividad."
+const val dato35Marcas = "Subway fue creado por un chico de 17 años para ayudar a pagar la universidad."
+const val dato36Marcas = "Nestlé empezó como una empresa de leche en polvo para bebés."
+const val dato37Marcas = "Unilever tiene más de 400 marcas, pero solo 14 generan la mitad de sus ingresos."
+const val dato38Marcas = "Harley-Davidson tiene varias fragancias oficiales de perfume."
+const val dato39Marcas = "Colgate vendía jabones antes de vender pasta dental."
+const val dato40Marcas = "Gucci empezó como una tienda de equipaje para jinetes y viajeros."
+const val dato41Marcas = "Doritos se inventó cuando un empleado fritó tortillas sobrantes en Disneyland."
+const val dato42Marcas = "Uber inicialmente se llamaba UberCab y solo ofrecía autos negros de lujo."
+const val dato43Marcas = "Coca-Cola vendió tan sólo 50 dólares el primer año."
+const val dato44Marcas = "El logo de Apple tiene una mordida para no confundirlo con una cereza."
+const val dato45Marcas = "Volkswagen significa \"auto del pueblo\"."
+const val dato46Marcas = "Coca-Cola fue verde al principio."
+const val dato47Marcas = "Chupa Chups tuvo su logo diseñado por Salvador Dalí."
+const val dato48Marcas = "Netflix originalmente enviaba solo DVDs por correo."
+const val dato49Marcas = "El nombre de Barbie es Barbara Millicent Roberts."
+const val dato50Marcas = "Tesla estuvo a punto de llamarse \"Faraday\"."
+const val dato51Marcas = "Ben & Jerry’s fue fundado por dos amigos que se conocieron en secundaria."
+const val dato52Marcas = "Calvin Klein comenzó como una pequeña tienda de abrigos en un hotel."
+const val dato53Marcas = "Rolex eligió su nombre porque era corto y sonaba igual en todos los idiomas."
+const val dato54Marcas = "Old Spice originalmente era un perfume para mujeres."
+const val dato55Marcas = "Häagen-Dazs no significa nada, inventaron el nombre para sonar premium."
+
+val datosMarcas = listOf(dato1Marcas, dato2Marcas, dato3Marcas, dato4Marcas, dato5Marcas,
+    dato6Marcas, dato7Marcas, dato8Marcas, dato9Marcas, dato10Marcas,
+    dato11Marcas, dato12Marcas, dato13Marcas, dato14Marcas, dato15Marcas,
+    dato16Marcas, dato17Marcas, dato18Marcas, dato19Marcas, dato20Marcas,
+    dato21Marcas, dato22Marcas, dato23Marcas, dato24Marcas, dato25Marcas,
+    dato26Marcas, dato27Marcas, dato28Marcas, dato29Marcas, dato30Marcas,
+    dato31Marcas, dato32Marcas, dato33Marcas, dato34Marcas, dato35Marcas,
+    dato36Marcas, dato37Marcas, dato38Marcas, dato39Marcas, dato40Marcas,
+    dato41Marcas, dato42Marcas, dato43Marcas, dato44Marcas, dato45Marcas,
+    dato46Marcas, dato47Marcas, dato48Marcas, dato49Marcas, dato50Marcas,
+    dato51Marcas, dato52Marcas, dato53Marcas, dato54Marcas, dato55Marcas)
