b187a8b29e0406bdea798b657341c87e12a603b1
xCrixz <sharzapps@gmail.com>
2025-11-21 08:14:02 -0500

    Se agregarón 54 nuevos datos para la categoria Misterios

--- app/src/main/java/com/sharkzapps/datatrove/pantallas/categorias/categoriaMisterios.kt
+++ app/src/main/java/com/sharkzapps/datatrove/pantallas/categorias/categoriaMisterios.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import com.sharkzapps.datatrove.pantallas.Encabezado
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.sharkzapps.datatrove.pantallas.datos.datosMisterios
 
 @Composable
 fun CategoriaMisterios(navController: NavController? = null) {
@@ -42,7 +43,9 @@ fun CategoriaMisterios(navController: NavController? = null) {
             )
     ) {
         Column(
-            modifier = Modifier.fillMaxSize().padding(14.dp, 8.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(14.dp, 8.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             val controller = navController ?: rememberNavController()
@@ -52,11 +55,15 @@ fun CategoriaMisterios(navController: NavController? = null) {
                 fontSize = 35.sp,
                 style = TextStyle(fontFamily = garamondFamily, fontWeight = FontWeight.Black),
                 modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Start)
-            HorizontalDivider(modifier = Modifier.height(5.dp)
+            HorizontalDivider(modifier = Modifier
+                .height(5.dp)
                 .width(230.dp)
                 .align(Alignment.Start),
                 thickness = 3.5.dp,
                 color = Color.Black)
+            
+            Spacer(modifier = Modifier.height(80.dp))
+            DatoAleatorio(datos = datosMisterios)
         }
     }
 }
--- app/src/main/java/com/sharkzapps/datatrove/pantallas/datos/DatosMisterios.kt
+++ app/src/main/java/com/sharkzapps/datatrove/pantallas/datos/DatosMisterios.kt
@@ -1,2 +1,107 @@
 package com.sharkzapps.datatrove.pantallas.datos
 
+const val dato1Misterios = "La \"zona del silencio\" en México es famosa porque radios y brújulas " +
+            "fallan sin razón aparente."
+const val dato2Misterios = "En Japón existe el pueblo de Nagoro, donde muñecos reemplazan a personas " +
+            "fallecidas o desaparecidas."
+const val dato3Misterios = "El Manuscrito Voynich sigue sin ser descifrado después de más de 600 años."
+const val dato4Misterios = "Los bosques de Aokigahara generan silencios tan profundos que afectan la " +
+            "orientación humana."
+const val dato5Misterios = "El misterio de Roanoke dejó a toda una colonia desaparecida sin ningún rastro."
+const val dato6Misterios = "En Rusia, el paso Dyatlov dejó nueve excursionistas muertos en " +
+            "circunstancias inexplicables."
+const val dato7Misterios = "El farero de Eilean Mor desapareció junto con otros dos hombres " +
+            "dejando la mesa servida."
+const val dato8Misterios = "La nave fantasma Mary Celeste fue encontrada sin tripulación y sin " +
+            "señales de violencia."
+const val dato9Misterios = "Las esferas de piedra de Costa Rica son perfectamente esféricas sin " +
+            "saber cómo fueron hechas."
+const val dato10Misterios = "El Puente Overtoun en Escocia ha hecho que cientos de perros " +
+            "salten sin explicación."
+const val dato11Misterios = "La esfera Betz actuaba como si estuviera viva, moviéndose sola por la casa."
+const val dato12Misterios = "El monstruo del Lago Ness sigue sin ser desmentido definitivamente " +
+            "pese a análisis modernos."
+const val dato13Misterios = "Los cráneos alargados de Paracas tienen características biológicas " +
+            "difíciles de explicar."
+const val dato14Misterios = "Las Luces de Marfa, en Texas, aparecen desde 1880 sin causa científica clara."
+const val dato15Misterios = "El objeto Oumuamua, que atravesó nuestro sistema solar en 2017, " +
+            "se comportaba como una nave sin motor."
+const val dato16Misterios = "El sonido \"Bloop\", fue tan fuerte que parecía provenir de una " +
+            "criatura gigantesca… pero nunca se halló."
+const val dato17Misterios = "El Triángulo del Dragón en Japón tiene desapariciones tan extrañas como " +
+            "el Triángulo de las Bermudas."
+const val dato18Misterios = "El monolito metálico de Utah apareció de la nada en 2020 y desapareció " +
+            "igual de misteriosamente."
+const val dato19Misterios = "El caso del Hombre de Somerton, hallado muerto con un código en el bolsillo, " +
+            "sigue sin resolverse desde 1948."
+const val dato20Misterios = "En Turquía existe una ciudad subterránea llamada Derinkuyu, con " +
+            "capacidad para 20.000 personas."
+const val dato21Misterios = "El misterio del vuelo MH370 es una de las desapariciones modernas más " +
+            "grandes sin explicación definitiva."
+const val dato22Misterios = "Las momias del Tarim tienen rasgos europeos pese a encontrarse " +
+            "en el corazón de China."
+const val dato23Misterios = "El caso del niño de la caja sigue sin resolución tras más de 60 años."
+const val dato24Misterios = "Los petroglifos de Marcahuasi parecen representar seres gigantes y figuras no humanas."
+const val dato25Misterios = "La pirámide sumergida de Yonaguni en Japón podría ser una ciudad perdida bajo el mar."
+const val dato26Misterios = "El \"Time Slip\" de Liverpool, donde múltiples personas dicen haber visto el pasado, " +
+            "sigue sin explicación científica."
+const val dato27Misterios = "El monstruo del Lago Champlain tiene más de 300 reportes visuales documentados."
+const val dato28Misterios = "El Libro de Soyga, estudiado por John Dee, tiene páginas indescifrables incluso hoy."
+const val dato29Misterios = "El piloto Frederick Valentich desapareció tras informar que lo seguía " +
+            "un objeto no identificado."
+const val dato30Misterios = "Las pirámides de Teotihuacán están alineadas con precisión astronómica."
+const val dato31Misterios = "Las esferas \"klerksdorp\" de Sudáfrica parecen objetos metálicos perfectos, " +
+            "pero tienen 3 mil millones de años."
+const val dato32Misterios = "El mecanismo de Antikythera, es tan avanzado que parece una computadora analógica " +
+            "creada hace miles de años."
+const val dato33Misterios = "En Siberia, el cráter Patomski sigue sin explicación clara."
+const val dato34Misterios = "En Escocia, el monstruo del Lago Ness tiene reportes que datan del siglo VI."
+const val dato35Misterios = "En Rusia, la Puerta al Inframundo sigue creciendo sin explicación clara."
+const val dato36Misterios = "El manuscrito Rohonc, igual que el Voynich, está escrito en un lenguaje " +
+            "y alfabeto desconocidos."
+const val dato37Misterios = "En la isla de Pascua, muchos moáis tienen cuerpos completos enterrados bajo tierra."
+const val dato38Misterios = "En Noruega, las luces de Hessdalen aparecen desde hace décadas con patrones inteligentes."
+const val dato39Misterios = "A viajeros en Escocia se les aparece un pueblo fantasma con arquitectura victoriana " +
+            "que desaparece al intentar acercarse."
+const val dato40Misterios = "El misterio de Taured cuenta la historia de un hombre que llegó con un pasaporte " +
+            "de un país inexistente."
+const val dato41Misterios = "En Perú, las líneas de Nazca fueron diseñadas para verse solo desde el aire, " +
+            "pero no se sabe el propósito."
+const val dato42Misterios = "El mar de los Sargazos ha sido escenario de múltiples desapariciones desde hace siglos."
+const val dato43Misterios = "Los bosques torcidos de Polonia tienen más de 400 árboles con curvaturas idénticas " +
+            "y nadie sabe por qué crecieron así."
+const val dato44Misterios = "En México, la Isla de las Muñecas presenta muñecas que supuestamente se mueven y " +
+            "cambian de posición sin intervención humana."
+const val dato45Misterios = "En Italia, la Cripta de los Capuchinos tiene cuerpos momificados tan bien " +
+            "conservados que parecen dormidos."
+const val dato46Misterios = "Los círculos de hadas de Namibia son formaciones perfectas en el suelo cuyo " +
+            "origen se sigue debatiendo."
+const val dato47Misterios = "En Francia, una casa conocida como la \"Maison Qui Saigne\" supuestamente " +
+            "expulsaba sangre de las paredes."
+const val dato48Misterios = "Los Cascos Dorados Celtas encontrados en Europa parecen más decorativos que funcionales, " +
+            "pero su uso real es desconocido."
+const val dato49Misterios = "El \"Hombre de Sombrero\", una figura oscura reportada en sueños alrededor del mundo, " +
+            "aparece con descripciones idénticas."
+const val dato50Misterios = "En el Ártico, varias expediciones han reportado ver ciudades espejadas en el cielo, " +
+            "un fenómeno aún no explicado del todo."
+const val dato51Misterios = "Hay pirámides en Sudán más numerosas que las de Egipto, pero su propósito " +
+            "sigue siendo un misterio."
+const val dato52Misterios = "El \"Manuscrito de Oera Linda\" podría describir una civilización avanzada desaparecida, " +
+            "pero nadie ha probado su autenticidad."
+const val dato53Misterios = "En Brasil, la Luz de Colares atacó físicamente a habitantes en los años 70 y nunca " +
+            "se identificó su origen."
+const val dato54Misterios = "El \"Pilar de Delhí\", hecho de hierro casi puro, no se oxida después de 1,600 años."
+
+val datosMisterios = listOf(
+    dato1Misterios, dato2Misterios, dato3Misterios, dato4Misterios, dato5Misterios,
+    dato6Misterios, dato7Misterios, dato8Misterios, dato9Misterios, dato10Misterios,
+    dato11Misterios, dato12Misterios, dato13Misterios, dato14Misterios, dato15Misterios,
+    dato16Misterios, dato17Misterios, dato18Misterios, dato19Misterios, dato20Misterios,
+    dato21Misterios, dato22Misterios, dato23Misterios, dato24Misterios, dato25Misterios,
+    dato26Misterios, dato27Misterios, dato28Misterios, dato29Misterios, dato30Misterios,
+    dato31Misterios, dato32Misterios, dato33Misterios, dato34Misterios, dato35Misterios,
+    dato36Misterios, dato37Misterios, dato38Misterios, dato39Misterios, dato40Misterios,
+    dato41Misterios, dato42Misterios, dato43Misterios, dato44Misterios, dato45Misterios,
+    dato46Misterios, dato47Misterios, dato48Misterios, dato49Misterios, dato50Misterios,
+    dato51Misterios, dato52Misterios, dato53Misterios, dato54Misterios)
+
