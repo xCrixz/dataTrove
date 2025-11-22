2bbb2ef26afabe1341a17d5623a080e83ba6ddcb
xCrixz <sharzapps@gmail.com>
2025-11-22 07:59:17 -0500

    Se agregarón 47 nuevos datos para la categoria Divertidos

--- app/src/main/java/com/sharkzapps/datatrove/pantallas/categorias/categoriaDivertidos.kt
+++ app/src/main/java/com/sharkzapps/datatrove/pantallas/categorias/categoriaDivertidos.kt
@@ -26,6 +26,7 @@ import com.sharkzapps.datatrove.pantallas.Encabezado
 import com.sharkzapps.datatrove.ui.theme.garamondFamily
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.sharkzapps.datatrove.pantallas.datos.datosDivertidos
 
 @Composable
 fun CategoriaDivertidos(navController: NavController? = null) {
@@ -42,7 +43,9 @@ fun CategoriaDivertidos(navController: NavController? = null) {
             )
     ) {
         Column(
-            modifier = Modifier.fillMaxSize().padding(14.dp, 8.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(14.dp, 8.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             val controller = navController ?: rememberNavController()
@@ -57,8 +60,14 @@ fun CategoriaDivertidos(navController: NavController? = null) {
                 textAlign = TextAlign.Start
             )
 
-            HorizontalDivider(modifier = Modifier.height(5.dp).width(230.dp).align(Alignment.Start),
+            HorizontalDivider(modifier = Modifier
+                .height(5.dp)
+                .width(230.dp)
+                .align(Alignment.Start),
                 thickness = 3.5.dp, color = Color.Black)
+            
+            Spacer(modifier = Modifier.height(80.dp))
+            DatoAleatorio(datos = datosDivertidos)
         }
     }
 }
--- app/src/main/java/com/sharkzapps/datatrove/pantallas/datos/DatosDivertidos.kt
+++ app/src/main/java/com/sharkzapps/datatrove/pantallas/datos/DatosDivertidos.kt
@@ -1,2 +1,70 @@
 package com.sharkzapps.datatrove.pantallas.datos
 
+const val dato1Divertidos = "Los perezosos pueden aguantar la respiración más tiempo que los " +
+            "delfines, hasta 40 minutos."
+const val dato2Divertidos = "En 2016, un hombre llamó a la policía porque su gato se sentó en su pizza."
+const val dato3Divertidos = "Existe un tipo de medusa que puede regresar a ser bebé cuando envejece."
+const val dato4Divertidos = "Las tortugas pueden respirar por el trasero… literalmente."
+const val dato5Divertidos = "En Japón existe un museo dedicado exclusivamente a los fideos instantáneos."
+const val dato6Divertidos = "Los astronautas del Apolo 11 tenían que firmar un formulario de aduana " +
+            "al regresar a la Tierra."
+const val dato7Divertidos = "El creador del autocorrector murió, pero su funeral fue muy emoticono."
+const val dato8Divertidos = "Hay un idioma ficticio llamado \"Bork Bork Bork\" basado en los Muppets, " +
+            "y Google lo tuvo como opción real."
+const val dato9Divertidos = "En 1998, Burger King lanzó una hamburguesa para zurdos."
+const val dato10Divertidos = "En Corea del Sur existe un parque temático dedicado al popó."
+const val dato11Divertidos = "El creador del GIF dice que se pronuncia \"yif\", y la mitad del " +
+            "planeta prefiere ignorarlo."
+const val dato12Divertidos = "Un pueblo de Inglaterra se llama \"Pity Me\" (Apiádense de mí)."
+const val dato13Divertidos = "El día que se inventó la galleta Oreo, también se inventó la " +
+            "versión falsa y nadie recuerda el nombre."
+const val dato14Divertidos = "Durante años, las papas Pringles no eran consideradas papas legalmente."
+const val dato15Divertidos = "En Islandia hay más ovejas que personas, pero también más libros publicados " +
+            "por habitante que en cualquier otro país."
+const val dato16Divertidos = "La Torre Eiffel fue odiada por casi todos los parisinos cuando la construyeron."
+const val dato17Divertidos = "El creador de la dinamita inventó también los premios Nobel."
+const val dato18Divertidos = "En un pueblo de Noruega está prohibido morir. Sí, prohibido."
+const val dato19Divertidos = "El reloj más pequeño del mundo cabe en la punta de un cabello."
+const val dato20Divertidos = "En el siglo XVII, los médicos recetaban \"chocolate caliente\" como medicina."
+const val dato21Divertidos = "Las primeras galletas de la fortuna NO se inventaron en China sino en California."
+const val dato22Divertidos = "Durante años, el llavero más vendido en Japón era un grano de arroz " +
+            "con tu nombre escrito"
+const val dato23Divertidos = "La primera webcam se usó para vigilar una cafetera en Cambridge."
+const val dato24Divertidos = "Google Earth encontró más piscinas privadas que cualquier gobierno."
+const val dato25Divertidos = "El creador del meme \"Trollface\" ganó más de 100 mil dólares por derechos de autor."
+const val dato26Divertidos = "El ketchup se vendía como medicina en 1830."
+const val dato27Divertidos = "Los emojis se crearon en 1999, pero nadie los usaba porque parecían dibujos de paint."
+const val dato28Divertidos = "En Japón existe un museo dedicado solo a las rocas que parecen caras."
+const val dato29Divertidos = "En Francia existe una ley que prohíbe llamar a un cerdo \"Napoleón\"."
+const val dato30Divertidos = "En Dinamarca hay un puente para bicicletas que parece una montaña rusa."
+const val dato31Divertidos = "El teléfono más vendido de la historia sigue siendo el Nokia 1100."
+const val dato32Divertidos = "En 2011, un mono robó una cámara y se tomó selfies"
+const val dato33Divertidos = "En Inglaterra existe una competencia de rodar un queso colina abajo."
+const val dato34Divertidos = "El inventor del frisbee pidió que conviertan sus cenizas en frisbees."
+const val dato35Divertidos = "En 1800, la gente creía que bostezar demasiado podía acortar tu vida."
+const val dato36Divertidos = "En Suiza, hay un hotel sin paredes ni techo."
+const val dato37Divertidos = "Las primeras máquinas expendedoras vendían agua bendita."
+const val dato38Divertidos = "Los carritos del supermercado fueron rechazados al inicio porque la gente " +
+            "se avergonzaba de usarlos."
+const val dato39Divertidos = "Las gorras de béisbol originalmente eran para proteger del sol dentro del estadio."
+const val dato40Divertidos = "En Japón existe una isla llena de conejos llamada Okunoshima."
+const val dato41Divertidos = "En Estados Unidos hay competencias de lanzar tortillas."
+const val dato42Divertidos = "En Suiza puedes alquilar una cabra para que te acompañe a caminar."
+const val dato43Divertidos = "El creador de Minecraft empezó el juego sin poder saltar."
+const val dato44Divertidos = "En 1886 se vendía \"agua eléctrica\" para mejorar el humor."
+const val dato45Divertidos = "En Francia hay un pueblo llamado \"Y\"."
+const val dato46Divertidos = "En 2017, una persona devolvió un libro a una biblioteca 123 años tarde."
+const val dato47Divertidos = "En 2019, un hombre entró a una comisaría para preguntar si tenían trabajo, " +
+            "pero tenía una orden de arresto activa."
+
+val datosDivertidos = listOf(
+    dato1Divertidos, dato2Divertidos, dato3Divertidos, dato4Divertidos, dato5Divertidos,
+    dato6Divertidos, dato7Divertidos, dato8Divertidos, dato9Divertidos, dato10Divertidos,
+    dato11Divertidos, dato12Divertidos, dato13Divertidos, dato14Divertidos, dato15Divertidos,
+    dato16Divertidos, dato17Divertidos, dato18Divertidos, dato19Divertidos, dato20Divertidos,
+    dato21Divertidos, dato22Divertidos, dato23Divertidos, dato24Divertidos, dato25Divertidos,
+    dato26Divertidos, dato27Divertidos, dato28Divertidos, dato29Divertidos, dato30Divertidos,
+    dato31Divertidos, dato32Divertidos, dato33Divertidos, dato34Divertidos, dato35Divertidos,
+    dato36Divertidos, dato37Divertidos, dato38Divertidos, dato39Divertidos, dato40Divertidos,
+    dato41Divertidos, dato42Divertidos, dato43Divertidos, dato44Divertidos, dato45Divertidos,
+    dato46Divertidos, dato47Divertidos)
