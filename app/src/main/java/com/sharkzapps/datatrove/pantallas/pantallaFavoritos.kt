ba894d7ee1466d5e66efa34779aa1d0c3a60c594
xCrixz <sharzapps@gmail.com>
2025-12-16 08:59:22 -0500

    Se mejoró la UX en la pantalla Favoritos, animación al borrar y doble click para eliminar favoritos

--- app/src/main/java/com/sharkzapps/datatrove/pantallas/pantallaFavoritos.kt
+++ app/src/main/java/com/sharkzapps/datatrove/pantallas/pantallaFavoritos.kt
@@ -1,6 +1,9 @@
 package com.sharkzapps.datatrove.pantallas
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,18 +14,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Card
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -35,9 +44,20 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.sharkzapps.datatrove.ui.theme.garamondFamily
+import kotlinx.coroutines.delay
 
 @Composable
 fun PantallaFavoritos(navController: NavController, state: CategoriaState){
+
+    var confirmarEliminar by remember { mutableStateOf<String?>(null) }
+
+
+    LaunchedEffect(confirmarEliminar) {
+        if (confirmarEliminar != null){
+            delay(2000)
+            confirmarEliminar = null
+        }
+    }
 
     Box(modifier = Modifier
         .fillMaxSize()
@@ -48,7 +68,8 @@ fun PantallaFavoritos(navController: NavController, state: CategoriaState){
                     Color(0xFFFFE082)
                 )
             )
-        )){
+        ))
+    {
         Column(modifier = Modifier
             .fillMaxSize()
             .padding(14.dp, 8.dp)) {
@@ -83,10 +104,20 @@ fun PantallaFavoritos(navController: NavController, state: CategoriaState){
                 }
             } else {
                 LazyColumn(modifier = Modifier.fillMaxSize(), contentPadding = PaddingValues(bottom = 37.dp)) {
-                    items(state.favoritos){ item -> Card(modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 6.dp),
-                        shape = RoundedCornerShape(12.dp)
+                    items(items =state.favoritos, key = {it} )
+                    { item ->
+                        val seleccionado = confirmarEliminar == item
+                        val bordeColor by animateColorAsState(
+                            targetValue = if(seleccionado) Color.Red else Color.Transparent,
+                            label = "borde")
+
+                        Card(modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 6.dp)
+                            .animateItem(),
+                        shape = RoundedCornerShape(12.dp), border = BorderStroke(
+                            width = if (seleccionado) 4.dp else 0.dp, color = bordeColor
+                        )
                     ) {
                         Row(modifier = Modifier
                             .fillMaxWidth()
@@ -95,15 +126,29 @@ fun PantallaFavoritos(navController: NavController, state: CategoriaState){
                             verticalAlignment = Alignment.CenterVertically) {
                             Text(
                                 text = item,
-                                fontSize = 22.sp,
+                                fontSize = 24.sp,
                                 fontFamily = garamondFamily,
                                 modifier = Modifier.weight(1f)
                             )
 
-                            IconButton(onClick = { state.cambiarFavorito(item) }) {
-                                Icon(imageVector = Icons.Default.Favorite,
-                                    contentDescription = "Quitar", tint = Color.Red )
-                            }
+                            Icon(
+                                imageVector = if (confirmarEliminar == item)
+                                    Icons.Default.Delete else Icons.Default.Favorite,
+                                contentDescription = "Quitar",
+                                tint = Color.Red,
+                                modifier = Modifier
+                                    .size(25.dp)
+                                    .clickable {
+                                        if (confirmarEliminar == item) {
+                                            state.cambiarFavorito(item)
+                                            confirmarEliminar = null
+                                        } else {
+                                            confirmarEliminar = item
+                                        }
+                                    }
+
+                            )
+
                         }
                     }}
                 }
